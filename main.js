910c139a7bbc850530e86428423de401e6309842
bazayd <brandonzarriaga@gmail.com>
2023-07-24 17:26:29 -0700

    css changes and js function

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("financeButton").onclick = function openCalc() {
+    window.open("https://stackoverflow.com/questions/6303964/javascript-open-a-given-url-in-a-new-tab-by-clicking-a-button", "_blank");
+}
