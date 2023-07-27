66d2dd15f4f8bee5c6b062e5c262f218bcbc92c1
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-07-27 15:57:53 -0700

    Added calculate function with functionality

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', calculate);
+
 
 
 function calculate() {
@@ -11,9 +11,17 @@ function calculate() {
     let wants = monthlySalary * 0.3;
     let savings = monthlySalary * 0.2;
 
+    let necessitiesContent = document.getElementById('necessitiesText');
+    let wantsContent = document.getElementById('wantsText');
+    let savingsContent = document.getElementById('savingsText');
 
-    alert ("Necessities: $" + necessities.toLocaleString() + "\nWants: $" + wants.toLocaleString() + "\n Savings: $" + savings.toLocaleString());
 
+    // alert ("Necessities: $" + necessities.toLocaleString() + "\nWants: $" + wants.toLocaleString() + "\nSavings: $" + savings.toLocaleString());
 
+    necessitiesContent.innerHTML = "Necessities : $" + necessities.toLocaleString();
+    wantsContent.innerHTML = "Wants: $" + wants.toLocaleString();
+    savingsContent.innerHTML = "Savings: $" + savings.toLocaleString();
 }
+
+submitButton.addEventListener('click', calculate);
 
